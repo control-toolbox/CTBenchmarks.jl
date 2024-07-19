28a9f03285d870d961d279c0f14f1d4170a2af70
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-07-19 10:50:39 +0200

    add MKL + correct data collection

--- Benchmark/utils.jl
+++ Benchmark/utils.jl
@@ -10,26 +10,3 @@ function prettytime(t)
     end
     return string(value , " " , units)
 end
-
-function jac_hess_nnz_JMP(model)
-    nlp = MOI.Nonlinear.Model()
-    for (F, S) in list_of_constraint_types(model)
-        if F <: VariableRef
-            continue
-        end
-        for ci in all_constraints(model, F, S)
-            object = constraint_object(ci)
-            MOI.Nonlinear.add_constraint(nlp, object.func, object.set)
-        end
-    end
-    MOI.Nonlinear.set_objective(nlp, objective_function(model))
-    evaluator = MOI.Nonlinear.Evaluator(
-        nlp,
-        MOI.Nonlinear.SparseReverseMode(),
-        index.(all_variables(model)),
-    )
-    MOI.initialize(evaluator, MOI.features_available(evaluator))
-    nnz_hess = length(MOI.hessian_lagrangian_structure(evaluator))
-    nnz_jac = length(MOI.jacobian_structure(evaluator))
-    return nnz_jac , nnz_hess
-end
