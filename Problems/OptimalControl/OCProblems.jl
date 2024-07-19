6b40f9caf8af15b83ec3a0e368ed7918ab5a1377
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-07-19 16:54:00 +0200

    set module JuMP and OC

--- Problems/OptimalControl/OCProblems.jl
+++ Problems/OptimalControl/OCProblems.jl
@@ -11,4 +11,20 @@ for file in files
     end
 end
 
+function_OC = Dict{Symbol, Function}()
+all_names = names(OCProblems, all=true)
+functions_list = filter(x -> isdefined(OCProblems, x) && isa(getfield(OCProblems, x), Function) && endswith(string(x), "_OC"), all_names)
+for f in functions_list
+    key = Symbol(lowercase(replace(string(f), "_OC" => "")))
+    function_OC[key] = OCProblems.eval(f)
+end
+
+function_init = Dict{Symbol, Function}()
+functions_list = filter(x -> isdefined(OCProblems, x) && isa(getfield(OCProblems, x), Function) && endswith(string(x), "_init"), all_names)
+for f in functions_list
+    key = Symbol(lowercase(replace(string(f), "_init" => "")))
+    function_init[key] = OCProblems.eval(f)
+end
+
+
 end
