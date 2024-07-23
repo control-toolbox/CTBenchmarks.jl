3dc9414a3be7ad7c3e65e1146dc6a064b1479090
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-07-23 10:53:49 +0200

    using init function

--- Problems/OptimalControl/rocket_OC.jl
+++ Problems/OptimalControl/rocket_OC.jl
@@ -62,10 +62,11 @@ function rocket_init(;nh)
     m_f = m_c * m_0
     T_max = T_c * m_0 * g_0
 
-    xinit = t -> [1.0,
-                t * (1.0 - t) ,
-                (m_f - m_0)*t + m_0]
+    xinit = [[1.0,
+              k* (1.0 - k) ,
+              (m_f - m_0) * k + m_0] for k=0:nh]
 
-    init = (state = xinit,control = T_max/2.0 ,variable= 1);
+    time_vec = LinRange(0.0,1.0,nh+1)
+    init = (time= time_vec, state = xinit , control = T_max/2.0 ,variable= 1);
     return init
 end
