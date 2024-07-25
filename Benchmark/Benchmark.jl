2d1223bcdd5e720e09b4fb2d27d241b2f98366f7
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-07-25 21:34:44 +0200

    add KNITRO benchmark

--- Benchmark/Benchmark.jl
+++ Benchmark/Benchmark.jl
@@ -15,8 +15,12 @@ include(path*"./Benchmark_OC.jl")
 include(path*"./Benchmark_JuMP.jl")
 include(path*"./Benchmark_model.jl")
 include(path*"./Benchmark_Callbacks.jl")
+include(path*"./Benchmark_Knitro.jl")
+include(path*"./display_functions.jl")
 
-using JuMP , Ipopt
+using JuMP 
+using Ipopt , KNITRO
+using ADNLPModels, NLPModelsKnitro
 using OptimalControl, NLPModelsIpopt
 import HSL_jll
 using NLPModels, NLPModelsJuMP
@@ -24,7 +28,7 @@ using NLPModels, NLPModelsJuMP
 using PrettyTables, Colors
 using DataFrames
 
-nb_discr_list = [100;500]
+nb_discr_list = [100; 500]
 excluded_models = [:space_shuttle; :quadrotor1obs; :quadrotorp2p; :truck; :moonlander; :glider]
 
 # JIT warm-up for the first run
@@ -44,106 +48,6 @@ finally
     redirect_stderr(original_stderr)
 end
 
-function uniflag(flag)
-    if flag == MOI.LOCALLY_SOLVED || flag == "Solve_Succeeded" || flag == MOI.OPTIMAL
-        return "Solve Succeeded"
-    elseif flag == MOI.ITERATION_LIMIT || flag == "Maximum_Iterations_Exceeded"
-        return "Iterations Exceeded"
-    elseif flag == MOI.LOCALLY_INFEASIBLE || flag == "Infeasible_Problem_Detected"
-        return "Infeasible Problem"
-    elseif flag == MOI.INVALID_MODEL || flag == "Invalid Model"
-        return "Invalid Model"
-    end
-    return "UNI ? "*string(flag)
-end
-
-function display_Benchmark(Results, title, file_name, parameter_value)
-    # Print the results
-    #println("---------- Results : ")
-    table = DataFrame(:Model => Symbol[], :nb_discr => Any[], :nvar => Any[], :ncon => Any[], :nb_iter => Any[], :total_time => Any[], :Ipopt_time => Any[], :obj_value => Any[], :flag => Any[])
-    ex=[]
-    for (k,v) in Results
-        if length(v) > 0
-            for i in v
-                push!(table, [k; i.nb_discr[1]; i.nvar; i.ncon; i.nb_iter[1]; round(i.total_time[1],digits=2); round(i.Ipopt_time[1],digits=2); i.obj_value[1]; uniflag(i.flag[1])])
-            end
-        else
-            push!(ex, [k])
-        end
-    end
-    for i in ex
-        push!(table, [i; NaN; NaN; NaN; NaN; NaN; NaN; NaN; "NaN"])
-    end
-    # Define the custom display
-    header = ["Model","Discretization", "Variables","Constraints", "Iterations", "Total Time", "Ipopt Time" ,"Objective Value", "Flag"];
-    hl_flags = LatexHighlighter( (table, i, j) -> ((j == 9) && (table[i, j] != "Solve Succeeded") && (table[i, j] != "NaN")),
-                            ["color{red}"]
-                        );
-    original_stdout = stdout
-    file = open("./outputs/$(file_name)", "w")
-    try
-        redirect_stdout(file)
-        println("\\documentclass{standalone}")
-        println("\\usepackage{color}")
-        println("\\usepackage{booktabs}")
-        println("\\begin{document}")
-        println("\\begin{tabular}{c}")
-        println("\\Large\\textbf{$title}\\\\")
-        println("\\large\\textbf{$parameter_value}\\\\")
-        pretty_table(
-            table;
-            (backend = Val(:latex)),
-            header        = header,
-            title = title,
-            title_alignment = :c,
-            alignment = :c,
-            highlighters  = (hl_flags,)
-        )
-        println("\\end{tabular}")
-        println("\\end{document}")
-    finally
-        redirect_stdout(original_stdout)
-        close(file)
-    end
-end 
-
-
-function display_Callbacks(Results, title, file_name)
-    table = DataFrame(:Model => Symbol[], :nb_discr => Any[], :nnzh => Any[], :nnzj => Any[], 
-                        :t_obj => Any[], :t_grad => Any[], :t_cons => Any[], :t_jac => Any[], :t_hess => Any[])
-    for (k,v) in Results
-        for i in v
-            push!(table, [k; i.nb_discr[1]; i.nnzh; i.nnzj; round(i.t_obj[1]*1e3,digits=2); round(i.t_grad[1]*1e3,digits=2); round(i.t_cons[1]*1e3,digits=2); round(i.t_jac[1]*1e3,digits=2); round(i.t_hess[1]*1e3,digits=2)])
-            #push!(table, [k; i.nb_discr[1]; i.nnzh; i.nnzj; i.t_obj[1]; i.t_grad[1]; i.t_cons[1]; i.t_jac[1]; i.t_hess[1]])
-        end
-    end
-    # Define the custom display
-    header = ["Model","Discretization", "nnz Hessian", "nnz Jacobian", "Time Obj(ms)", "Time Grad(ms)", "Time Cons(ms)", "Time Jac(ms)", "Time Hess(ms)"];
-    original_stdout = stdout
-    file = open("./outputs/$(file_name)", "w")
-    try
-        redirect_stdout(file)
-        println("\\documentclass{standalone}")
-        println("\\usepackage{color}")
-        println("\\usepackage{booktabs}")
-        println("\\begin{document}")
-        println("\\begin{tabular}{c}")
-        println("\\Large\\textbf{$title}\\\\")
-        pretty_table(
-            table;
-            (backend = Val(:latex)),
-            header        = header,
-            title = title,
-            title_alignment = :c,
-            alignment = :c,
-        )
-        println("\\end{tabular}")
-        println("\\end{document}")
-    finally
-        redirect_stdout(original_stdout)
-        close(file)
-    end
-end
 
 
 function Benchmark_OC(nb_discr_list=nb_discr_list, excluded_models=excluded_models;max_iter=1000, tol=1e-8, constr_viol_tol = 1e-6,solver="ma57",display=false)
@@ -176,6 +80,14 @@ function Benchmark_Callbacks(model_key, nb_discr_list=nb_discr_list)
     title = "Benchmark Callbacks of $model_key model with JuMP and OptimalControl"
     file_name = "Benchmark_Callbacks_file.tex"
     display_Callbacks(Results, title, file_name)
+end
+
+
+function Benchmark_KNITRO(model_key_list, nb_discr_list;display=false)
+    Results = benchmark_knitro(model_key_list, OCProblems.function_init , nb_discr_list;display=display)
+    title = "Benchmark Knitro Results"
+    file_name = "Knitro_Benchmark_file.tex"
+    display_Knitro(Results, title, file_name)
 end
 
 end
---
+++ Benchmark/Benchmark_Knitro.jl
@@ -0,0 +1,110 @@
+"""
+    This file contains the functions to benchmark the models using OptimalControl and JuMP
+"""
+
+# Function to solve the model with OptimalControl
+function benchmark_Knitro_OC(model_key, model, init, nb_discr;display=false)
+    nh = nb_discr - 1 > 1 ? nb_discr - 1 : 2 
+    if !KNITRO.has_knitro()
+        error("KNITRO is not available")
+    else
+        nlp = get_nlp(direct_transcription(model,grid_size=nb_discr, init=init))
+        if display
+            t = @timed ( 
+                stats = knitro(nlp)
+            )
+        else
+            t = @timed ( 
+                stats = knitro(nlp, outlev=0)
+            )
+        end
+    end
+    # Get the results
+    knitro_time = stats.elapsed_time
+    obj_value = stats.objective
+    flag = stats.solver_specific[:internal_msg]
+    nb_iter = stats.iter
+    total_time = t.time
+    nlp = get_nlp(direct_transcription(model; grid_size=nb_discr))
+    nvar = nlp.meta.nvar
+    ncon = nlp.meta.ncon
+    data = DataFrame(   :model => model_key,
+                        :nb_discr => nb_discr,
+                        :nvar => nvar,
+                        :ncon => ncon,
+                        :nb_iter => nb_iter,
+                        :obj_value => obj_value,
+                        :total_time => total_time,
+                        :knitro_time => knitro_time,
+                        :flag => flag)
+    return data
+end
+
+
+# Function to solve the model with JuMP
+function benchmark_Knitro_JuMP(model_key, model, nb_discr;display=false)
+    # Set up the solver
+    if !display
+        set_silent(model)
+    end
+    set_optimizer(model,KNITRO.Optimizer)
+    # Solve the model
+    t =  @timed (optimize!(model));
+    # Get the results
+    obj_value = JuMP.objective_value(model)
+    flag = solution_summary(model).termination_status
+    nb_iter = solution_summary(model).barrier_iterations
+    knitro_time = solution_summary(model).solve_time
+    total_time = t.time
+    nvar = MOI.get(model, MOI.NumberOfVariables());
+    ncon = length(all_constraints(model; include_variable_in_set_constraints = false))
+    data = DataFrame(   :model => model_key,
+                        :nb_discr => nb_discr,
+                        :nvar => nvar,
+                        :ncon => ncon,
+                        :nb_iter => nb_iter,
+                        :obj_value => obj_value,
+                        :total_time => total_time,
+                        :knitro_time => knitro_time,
+                        :flag => flag)
+    return data
+end
+
+
+# Function to benchmark the model
+function benchmark_knitro(model_key_list, inits , nb_discr_list;display=false)
+    Results = Dict{Symbol,Any}()
+    solve_OC = true
+    solve_JMP = true
+    R_OC = []
+    R_JuMP = []
+    for model_key in model_key_list
+        if ! (model_key in keys(OCProblems.function_OC))
+            println("The model $model_key is not available in the OptimalControl benchmark list. ❌")
+            solve_OC = false
+        end
+        if ! (model_key in keys(JMPProblems.function_JMP))
+            println("The model $model_key is not available in the JuMP benchmark list. ❌")
+            solve_JMP = false
+        end
+        for nb_discr in nb_discr_list
+            if solve_OC
+                print("Benchmarking the model $model_key with OptimalControl ($nb_discr)... ")
+                model = OCProblems.function_OC[model_key]()
+                info_OC = benchmark_Knitro_OC(model_key,model, inits[model_key](;nh=nb_discr-1), nb_discr;display=display)
+                push!(R_OC, info_OC)
+                println("✅")
+            end
+            if solve_JMP
+                print("Benchmarking the model $model_key with JuMP ($nb_discr)... ")
+                model = JMPProblems.function_JMP[model_key](;nh=nb_discr)
+                info_JuMP = benchmark_Knitro_JuMP(model_key,model, nb_discr;display=display)
+                push!(R_JuMP, info_JuMP)
+                println("✅")
+            end
+        end
+    end
+    Results[:JuMP] = R_JuMP
+    Results[:OptimalControl] = R_OC
+    return Results
+end
