d337038b8cf0a3e746c295fed6227032f9c51334
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-05-15 22:37:13 +0200

    test (bad) with ForwardDiff + GPU

--- kernel_test/foo.jl
+++ kernel_test/foo.jl
@@ -8,6 +8,7 @@ using AMDGPU
 using CUDA
 import CTParser: subs2
 using MLStyle
+import ForwardDiff: jacobian!
 
 function dynamics_k!(dx, x, u)
     N = size(dx, 2)
@@ -29,6 +30,49 @@ function dynamics!(dx, x, u) # user defined / generated
     dx[1] = u[1] * cosx12
     dx[2] = x[2] + x[3] * u[2]
     dx[3] = u[1] + cosx12
+    return nothing
+end
+
+function f_k!(y, x)
+    N = size(x, 2)
+    backend = KA.get_backend(x)
+    kernel = __f!(backend)
+    kernel(y, x, ndrange = N)
+    KA.synchronize(backend)
+    return
+end
+
+@kernel function __f!(y, @Const(x))
+    j = @index(Global, Linear)
+    f!(view(y, :, j), view(x, :, j))
+    nothing # no return allowed
+end
+
+function f!(y, x) # user defined / generated
+    cosx12 = cos(x[1] * x[2])
+    y[1] = cosx12
+    y[2] = x[2] + x[3]
+    y[3] = x[1] + x[2] * cosx12
+    return nothing
+end
+
+function df_k!(dy, y, x)
+    N = size(x, 2)
+    backend = KA.get_backend(x)
+    kernel = __df!(backend)
+    kernel(dy, y, x, ndrange = N)
+    KA.synchronize(backend)
+    return
+end
+
+@kernel function __df!(dy, y, @Const(x))
+    j = @index(Global, Linear)
+    df!(view(dy, :, :, j), view(y, :, j), view(x, :, j))
+    nothing # no return allowed
+end
+
+function df!(dy, y, x)
+    jacobian!(dy, f!, y, x)
     return nothing
 end
 
@@ -68,23 +112,35 @@ end
 
 n = 3
 m = 2
-N = Int(1e6)
+N = Int(1e7)
 
 # CPU kernel
 
+println("CPU:")
 x = rand(n, N)
 u = rand(m, N)
 dx = zeros(n, N)
 dynamics_k!(dx, x, u);
 @time dynamics_k!(dx, x, u)
 
+y = zeros(n, N)
+dy = zeros(n, n, N)
+df_k!(dy, y, x);
+@time df_k!(dy, y, x)
+
 # CUDA kernel
 
-d_x = CuMatrix(x)
-d_u = CuMatrix(u)
-d_dx = CuMatrix(dx)
+println("CUDA:")
+d_x = CuArray(x)
+d_u = CuArray(u)
+d_dx = CuArray(dx)
 dynamics_k!(d_dx, d_x, d_u);
 CUDA.@time dynamics_k!(d_dx, d_x, d_u)
+
+d_y = CuArray(y)
+d_dy = CuArray(dy)
+df_k!(dy, y, x);
+CUDA.@time df_k!(dy, y, x)
 
 # AMD kernel
 
