d08ac87ba471e3e19e0bbad0f15306c6b87655fc
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-10-09 22:57:20 +0200

    added mini.jl

--- src/mini.jl
+++ src/mini.jl
@@ -1,3 +1,5 @@
+# todo: check solver return status, number of iterations...
+
 using OptimalControlProblems
 using OptimalControl
 import JuMP
@@ -7,18 +9,18 @@ using MadNLPMumps
 using BenchmarkTools
 
 solver = :ipopt
-disc_method = :trapeze
-print_level = 0
-problem = :space_shuttle
+print_level = 0 # solver dependent
+disc_method = :trapeze # only available method for JuMP
+problem = :space_shuttle # should loop on problems
 
-for N ∈ [100, 500]
+for N ∈ [100, 200, 500, 1000]
 
     println("\nN    :   $N")
 
     # JuMP
     print("JuMP : ")
     nlp = eval(problem)(JuMPBackend(); grid_size=N)
-    JuMP.set_optimizer(nlp, Ipopt.Optimizer)
+    JuMP.set_optimizer(nlp, Ipopt.Optimizer) # update for MadNLP
     JuMP.set_optimizer_attribute(nlp, "print_level", print_level)
     @btime JuMP.optimize!($nlp)
     
