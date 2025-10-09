f36b11f178b486bd94250f1fa9499c5c18c476d8
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-10-09 23:06:39 +0200

    added mini.jl

--- src/mini.jl
+++ src/mini.jl
@@ -13,25 +13,27 @@ print_level = 0 # solver dependent
 disc_method = :trapeze # only available method for JuMP
 problem = :space_shuttle # should loop on problems
 
-for N ∈ [100, 200, 500, 1000]
+println("problem: $problem, solver: $solver, disc_method: $disc_method")
 
-    println("\nN    :   $N")
+for N ∈ [100, 200, 500]
+
+    println("\nN      :   $N")
 
     # JuMP
-    print("JuMP : ")
+    print("JuMP   : ")
     nlp = eval(problem)(JuMPBackend(); grid_size=N)
     JuMP.set_optimizer(nlp, Ipopt.Optimizer) # update for MadNLP
     JuMP.set_optimizer_attribute(nlp, "print_level", print_level)
     @btime JuMP.optimize!($nlp)
     
     # adnlp
-    print("adnlp: ")
+    print("adnlp  : ")
     docp = eval(Symbol(problem, :_s))(OptimalControlBackend(), :adnlp, solver; grid_size=N, disc_method=disc_method)
     nlp = nlp_model(docp)
     @btime eval(solver)($nlp; print_level=$print_level)
     
     # exa
-    print("exa  : ")
+    print("exa    : ")
     docp = eval(Symbol(problem, :_s))(OptimalControlBackend(), :exa, solver; grid_size=N, disc_method=disc_method)
     nlp = nlp_model(docp)
     @btime eval(solver)($nlp; print_level=$print_level)
